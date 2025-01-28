a4c56935b10a975e6388d4b01316a15c47b4a9ce
KrishaPandey <krishapandey2001@gmail.com>
2025-01-28 18:52:47 +0545

    delete

--- backend/api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import predict_view  # Import predict_view module
-
-urlpatterns = [
-    path('predict/', predict_view.predict_view, name='predict')  # Route for prediction
-]
